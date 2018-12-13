2d3f373232d08396becf9719c661e1ee2df31ae0
cyee00 <cyee00@stuy.edu>
2018-12-12 20:42:30 -0500

    l

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,18 +1,30 @@
 class Node{
   private int data;
   private Node next,prev;
+  public Node(int d,Node n, Node p){
+    data=d;
+    next=n;
+    if (p!=null){
+      prev=p;
+    }
+  }
 }
 
 public class MyLinkedList{
   private int size;
   private Node start,end;
+  public MyLinkedList(){}
   public int size(){
-    return 0;
+    return size;
   }
   public boolean add(int value){
+    size++;
+    Node end=new Node(value,null,end);
     return true;
   }
   public String toString(){
-    return "";
+    String ans="";
+    //for (
+    return ans;
   }
 }
